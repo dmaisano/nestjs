5101738b62bb069621a60de579b3b9cb4650af29
Domenico Maisano <dmaisanooo@gmail.com>
2021-11-22 22:01:32 -0500

    15:32 timestamp

--- typeorm-graphql-tutorial/src/app.module.ts
+++ typeorm-graphql-tutorial/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault as ApolloSandbox } from 'apollo-server-core';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: false,
+      plugins: [ApolloSandbox()],
+      autoSchemaFile: join(process.cwd(), `src/schema.gql`),
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
